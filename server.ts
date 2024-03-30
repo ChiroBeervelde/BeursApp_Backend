da4256b861e087c76f1749f299e06d9f22e9439e
ArneMoens <arne.ad.moens@outlook.com>
2024-03-30 22:03:56 +0100

    port boo boo fix

--- server.ts
+++ server.ts
@@ -6,13 +6,13 @@ dotenv.config();
 
 const app: Application = express();
 const server: Server = new Server(app);
-const PORT: number = process.env.PORT ? parseInt(9000, 10) : 3001;
+const PORT: number = process.env.PORT ? parseInt(process.env.PORT, 10) : 9000;
 
 
 
 app
   .listen(PORT, "localhost", function () {
-    console.log(`🚀[server]: Server is running at http://localhost:9000`);
+    console.log(`🚀[server]: Server is running at http://localhost:${PORT}`);
   })
   .on("error", (err: any) => {
     if (err.code === "EADDRINUSE") {
