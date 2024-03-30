5819c101bbbc9b5030005d170e977932a932309e
ArneMoens <arne.ad.moens@outlook.com>
2024-03-30 23:25:55 +0100

    cors

--- src/index.ts
+++ src/index.ts
@@ -11,9 +11,10 @@ export default class Server {
 
   private config(app: Application): void {
     const corsOptions: CorsOptions = {
-        origin: ["http://localhost:3000", "http://20.224.18.56:3000"]
+        origin: ["http://localhost:3000", "http://20.224.18.56:3000", "http://172.0.0.1:3000", "http://0.0.0.0:3000"]
     };
 
+    app.use(cors(corsOptions));
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
   }
