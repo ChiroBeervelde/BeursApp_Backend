7a7fbdfad07362f2fbdd50b0a86c5d16071169f9
Arne Moens <72036748+ArneMoens@users.noreply.github.com>
2024-04-03 09:44:13 +0200

    Update dranken.sql

--- database_mysql_files/mock_data_instert/dranken.sql
+++ database_mysql_files/mock_data_instert/dranken.sql
@@ -1,41 +1,41 @@
 -- Inserting data into the "dranken" table
 
--- Coca-Cola
+-- Pils Haeseveld
 INSERT INTO dranken (naam, huidigePrijs, vorigePrijs, minPrijs, maxPrijs, categorie, alcoholisch, kleur, afbeelding, hotkey)
-VALUES ('Coca-Cola', 2.5, 2.25, 2, 5, 'frisdrank', false, '#000000', '/images/logos/coca_cola-logo.jpg', 'c');
-
--- Stella Artois
-INSERT INTO dranken (naam, huidigePrijs, vorigePrijs, minPrijs, maxPrijs, categorie, alcoholisch, kleur, afbeelding, hotkey)
-VALUES ('Stella Artois', 3.5, 3.25, 2, 5, 'bier', true, '#ffff00', '/images/logos/stella_artois-logo.jpg', 'a');
+VALUES ('Pils Haeseveld', 2, 0, 1.25, 5, 'bier', true, '(107, 70, 6)', '/images/logos/heaseveld_logo.jpg', '1');
 
 -- Duvel
 INSERT INTO dranken (naam, huidigePrijs, vorigePrijs, minPrijs, maxPrijs, categorie, alcoholisch, kleur, afbeelding, hotkey)
-VALUES ('Duvel', 4.5, 4.25, 2, 5, 'bier', true, '#ff0000', '/images/logos/duvel-logo.png', 'd');
+VALUES ('Duvel', 4, 0, 2, 7.5, 'bier', true, '(245, 10, 10)', '/images/logos/heaseveld_logo.jpg', '2');
 
--- Fanta
+-- Strong Blond
 INSERT INTO dranken (naam, huidigePrijs, vorigePrijs, minPrijs, maxPrijs, categorie, alcoholisch, kleur, afbeelding, hotkey)
-VALUES ('Fanta', 2.25, 2, 2, 5, 'frisdrank', false, '#ff6600', '/images/logos/fanta-logo.png', 'f');
+VALUES ('Strong Blond', 5, 0, 3, 9, 'bier', true, '(235, 180, 52)', '/images/logos/heaseveld_logo.jpg', '3');
 
--- Jupiler
+-- Sommersby
 INSERT INTO dranken (naam, huidigePrijs, vorigePrijs, minPrijs, maxPrijs, categorie, alcoholisch, kleur, afbeelding, hotkey)
-VALUES ('Jupiler', 3.75, 3.5, 2, 5, 'bier', true, '#ffcc00', '/images/logos/jupiler-logo.jpeg', 'j');
+VALUES ('Sommersby', 3, 0, 2, 7.5, 'bier', true, '(141, 237, 133)', '/images/logos/heaseveld_logo.jpg', '4');
 
--- Leffe
+-- Tripel Karmeliet
 INSERT INTO dranken (naam, huidigePrijs, vorigePrijs, minPrijs, maxPrijs, categorie, alcoholisch, kleur, afbeelding, hotkey)
-VALUES ('Leffe', 4.75, 4.5, 2, 5, 'bier', true, '#663300', '/images/logos/leffe-logo.png', 'l');
+VALUES ('Tripel Karmeliet', 4, 0, 2, 7.5, 'bier', true, '(245, 107, 2)', '/images/logos/heaseveld_logo.jpg', '5');
 
--- Sprite
+-- Omer
 INSERT INTO dranken (naam, huidigePrijs, vorigePrijs, minPrijs, maxPrijs, categorie, alcoholisch, kleur, afbeelding, hotkey)
-VALUES ('Sprite', 2.25, 2, 2, 5, 'frisdrank', false, '#00ff00', '/images/logos/sprite-logo.jpg', 's');
+VALUES ('Omer', 4, 0, 2, 7.5, 'bier', true, '(7, 5, 8)', '/images/logos/heaseveld_logo.jpg', '6');
 
--- Hoegaarden
+-- Chouffe
 INSERT INTO dranken (naam, huidigePrijs, vorigePrijs, minPrijs, maxPrijs, categorie, alcoholisch, kleur, afbeelding, hotkey)
-VALUES ('Hoegaarden', 4, 3.75, 2, 5, 'bier', true, '#ffffff', '/images/logos/hoegaarden-logo.png', 'h');
+VALUES ('Chouffe', 4, 0, 2, 7.5, 'bier', true, '(173, 4, 219)', '/images/logos/heaseveld_logo.jpg', '7');
 
--- Chaudfontaine
+-- Chimay Blauw
 INSERT INTO dranken (naam, huidigePrijs, vorigePrijs, minPrijs, maxPrijs, categorie, alcoholisch, kleur, afbeelding, hotkey)
-VALUES ('Chaudfontaine', 2.75, 2.5, 2, 5, 'water', false, '#0000ff', '/images/logos/chaudfontaine-logo.png', 'c');
+VALUES ('Chimay Blauw', 4, 0, 2, 7.5, 'bier', true, '(44, 56, 212)', '/images/logos/heaseveld_logo.jpg', '8');
 
--- Orval
+-- Carlsberg
 INSERT INTO dranken (naam, huidigePrijs, vorigePrijs, minPrijs, maxPrijs, categorie, alcoholisch, kleur, afbeelding, hotkey)
-VALUES ('Orval', 5, 4.75, 2, 5, 'bier', true, '#ff9900', '/images/logos/orval-logo.jpeg', 'o');
+VALUES ('Carlsberg', 3, 0, 1.5, 5, 'bier', true, '(10, 130, 16)', '/images/logos/heaseveld_logo.jpg', '9');
+
+-- Kasteelbier Rouge
+INSERT INTO dranken (naam, huidigePrijs, vorigePrijs, minPrijs, maxPrijs, categorie, alcoholisch, kleur, afbeelding, hotkey)
+VALUES ('Kasteelbier Rouge', 4, 0, 2, 7.5, 'bier', true, '(130, 10, 32)', '/images/logos/heaseveld_logo.jpg', '0');
