e68844b513b275b613c4b77c0a1120510f671169
ArneMoens <arne.ad.moens@outlook.com>
2024-04-03 22:16:05 +0200

    update dranken

--- database_mysql_files/mock_data_instert/dranken.sql
+++ database_mysql_files/mock_data_instert/dranken.sql
@@ -2,11 +2,11 @@
 
 -- Pils Haeseveld
 INSERT INTO dranken (naam, huidigePrijs, vorigePrijs, minPrijs, maxPrijs, categorie, alcoholisch, kleur, afbeelding, hotkey)
-VALUES ('Pils Haeseveld', 2, 0, 1.25, 5, 'bier', true, '(107, 70, 6)', '/images/logos/heaseveld_logo.jpg', '1');
+VALUES ('Pils Haeseveld', 2, 0, 1.25, 5, 'bier', true, '(107, 70, 6)', 'images/logos/haeseveld_logo.jpeg', '1');
 
 -- Duvel
 INSERT INTO dranken (naam, huidigePrijs, vorigePrijs, minPrijs, maxPrijs, categorie, alcoholisch, kleur, afbeelding, hotkey)
-VALUES ('Duvel', 4, 0, 2, 7.5, 'bier', true, '(245, 10, 10)', '/images/logos/heaseveld_logo.jpg', '2');
+VALUES ('Duvel', 4, 0, 2, 7.5, 'bier', true, '(245, 10, 10)', '/images/logos/duvel_logo.jpg', '2');
 
 -- Strong Blond
 INSERT INTO dranken (naam, huidigePrijs, vorigePrijs, minPrijs, maxPrijs, categorie, alcoholisch, kleur, afbeelding, hotkey)
@@ -14,28 +14,28 @@ VALUES ('Strong Blond', 5, 0, 3, 9, 'bier', true, '(235, 180, 52)', '/images/log
 
 -- Sommersby
 INSERT INTO dranken (naam, huidigePrijs, vorigePrijs, minPrijs, maxPrijs, categorie, alcoholisch, kleur, afbeelding, hotkey)
-VALUES ('Sommersby', 3, 0, 2, 7.5, 'bier', true, '(141, 237, 133)', '/images/logos/heaseveld_logo.jpg', '4');
+VALUES ('Sommersby', 3, 0, 2, 7.5, 'bier', true, '(141, 237, 133)', '/images/logos/somersby_logo.jpg', '4');
 
 -- Tripel Karmeliet
 INSERT INTO dranken (naam, huidigePrijs, vorigePrijs, minPrijs, maxPrijs, categorie, alcoholisch, kleur, afbeelding, hotkey)
-VALUES ('Tripel Karmeliet', 4, 0, 2, 7.5, 'bier', true, '(245, 107, 2)', '/images/logos/heaseveld_logo.jpg', '5');
+VALUES ('Tripel Karmeliet', 4, 0, 2, 7.5, 'bier', true, '(245, 107, 2)', '/images/logos/tripel_karmeliet_logo.jpg', '5');
 
 -- Omer
 INSERT INTO dranken (naam, huidigePrijs, vorigePrijs, minPrijs, maxPrijs, categorie, alcoholisch, kleur, afbeelding, hotkey)
-VALUES ('Omer', 4, 0, 2, 7.5, 'bier', true, '(7, 5, 8)', '/images/logos/heaseveld_logo.jpg', '6');
+VALUES ('Omer', 4, 0, 2, 7.5, 'bier', true, '(7, 5, 8)', '/images/logos/omer_logo.jpg', '6');
 
 -- Chouffe
 INSERT INTO dranken (naam, huidigePrijs, vorigePrijs, minPrijs, maxPrijs, categorie, alcoholisch, kleur, afbeelding, hotkey)
-VALUES ('Chouffe', 4, 0, 2, 7.5, 'bier', true, '(173, 4, 219)', '/images/logos/heaseveld_logo.jpg', '7');
+VALUES ('La Chouffe', 4, 0, 2, 7.5, 'bier', true, '(173, 4, 219)', '/images/logos/la_chouffe_logo.jpg', '7');
 
 -- Chimay Blauw
 INSERT INTO dranken (naam, huidigePrijs, vorigePrijs, minPrijs, maxPrijs, categorie, alcoholisch, kleur, afbeelding, hotkey)
-VALUES ('Chimay Blauw', 4, 0, 2, 7.5, 'bier', true, '(44, 56, 212)', '/images/logos/heaseveld_logo.jpg', '8');
+VALUES ('Chimay Blauw', 4, 0, 2, 7.5, 'bier', true, '(44, 56, 212)', '/images/logos/chimay_blauw_logo.jpg', '8');
 
 -- Carlsberg
 INSERT INTO dranken (naam, huidigePrijs, vorigePrijs, minPrijs, maxPrijs, categorie, alcoholisch, kleur, afbeelding, hotkey)
-VALUES ('Carlsberg', 3, 0, 1.5, 5, 'bier', true, '(10, 130, 16)', '/images/logos/heaseveld_logo.jpg', '9');
+VALUES ('Carlsberg', 3, 0, 1.5, 5, 'bier', true, '(10, 130, 16)', '/images/logos/carlsberg_logo.jpg', '9');
 
 -- Kasteelbier Rouge
 INSERT INTO dranken (naam, huidigePrijs, vorigePrijs, minPrijs, maxPrijs, categorie, alcoholisch, kleur, afbeelding, hotkey)
-VALUES ('Kasteelbier Rouge', 4, 0, 2, 7.5, 'bier', true, '(130, 10, 32)', '/images/logos/heaseveld_logo.jpg', '0');
+VALUES ('Kasteelbier Rouge', 4, 0, 2, 7.5, 'bier', true, '(130, 10, 32)', '/images/logos/kasteelbier_rouge_logo.jpg', '0');
